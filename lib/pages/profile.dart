5ffd7cc75710c1415f66f0a3950c470f079a5e30
andrewta999 <andrewta365@gmail.com>
2020-03-19 03:16:15 -0400

    added header widget to profile and timeline pages

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:photoshare/widgets/header.dart';
 
+//Profile page
 class Profile extends StatefulWidget {
   @override
   _ProfileState createState() => _ProfileState();
 }
 
+//Profile page's state
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Text("Profile");
+    return Scaffold(
+      //get app bar widget
+      appBar: header(context, titleText: "Profile"),
+      //body widget
+      body: Text("Profile"),
+    );
   }
 }
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:photoshare/widgets/header.dart';
+import 'package:photoshare/widgets/progress.dart';
 
+//Timeline page
 class Timeline extends StatefulWidget {
   @override
   _TimelineState createState() => _TimelineState();
 }
 
+//Timeline page's states
 class _TimelineState extends State<Timeline> {
   @override
   Widget build(context) {
-    return Text("Timeline");
+    return Scaffold(
+      //calls header() to get appBar widget
+      appBar: header(context, isAppTitle: true),
+      //calls linearProgress() to get linear progress widget
+      body: linearProgress(),
+    );
   }
 }
