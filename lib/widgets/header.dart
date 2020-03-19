7bf009c4f6e2a54c62b623621776a0173dca9c05
andrewta999 <andrewta365@gmail.com>
2020-03-19 03:11:49 -0400

    added progress and header widgets

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+//create an app bar header
+//isApptitle a boolean indicates if this is the main page
+//title of the page
+AppBar header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? "PhotoShare" : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? "Signatra" : "",
+        fontSize: isAppTitle ? 50.0 : 22.0,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
--- lib/widgets/progress.dart
+++ lib/widgets/progress.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
-circularProgress() {
-  return Text("circular progress");
+//create a circular progress icon
+//return a container which is the icon
+Container circularProgress() {
+  return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 10.0),
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.purple),
+      ));
 }
 
-linearProgress() {
-  return Text("linear progress");
+//create a linear progress icon
+//return a container which is the icon
+Container linearProgress() {
+  return Container(
+    padding: EdgeInsets.only(bottom: 10.0),
+    child: LinearProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.purple),
+    ),
+  );
 }
+
